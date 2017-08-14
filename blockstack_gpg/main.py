a3706565ec0956840e273f26f9bf22573eca741d
Valentin Sundermann <me@vsund.de>
2017-08-14 14:10:12 +0200

    Introduce own print methods to reduce code complexity

--- blockstack_gpg/main.py
+++ blockstack_gpg/main.py
@@ -28,14 +28,11 @@ def getKeys(accounts, bskId):
 
 	# Fail when cannot find formatted account data
 	if not "accounts" in accounts:
-		if not args.silent:
-			print("Couldn't find valid profile information for \"%s\"" % bskId)
-		if args.debug:
-			print("Accounts: %s" % accounts)
+		printInfo("Couldn't find valid profile information for \"%s\"" % bskId)
+		printDebug("Accounts: %s" % accounts)
 		return;
 
-	if args.debug:
-		print("%s's accounts: %s" % (bskId, accounts))
+	printDebug("%s's accounts: %s" % (bskId, accounts))
 
 	# Cycle through all accounts...
 	for account in accounts["accounts"]:
@@ -45,34 +42,30 @@ def getKeys(accounts, bskId):
 			if not args.printAll or not keyFound:
 				keyFound = True
 
-				if args.debug:
-					print("Identifier: %s" % account["identifier"])
-					print("Key URL   : %s" % account["contentUrl"])
+				printDebug("Identifier: %s" % account["identifier"])
+				printDebug("Key URL   : %s" % account["contentUrl"])
 
 				# Try to download from the given key url
 				try:
 					key = urllib2.urlopen(account["contentUrl"]).read()
 				except Exception:
-					if not args.silent:
-						print("Error while fetching key from \"%s\"" % account["contentUrl"])
+					printInfo("Error while fetching key from \"%s\"" % account["contentUrl"])
 					return;
 
 				# Print without check when dontVerify is True
 				if args.dontVerify:
-					if not args.silent:
-						print("PGP key for \"%s\":" % bskId)
+					printInfo("PGP key for \"%s\":" % bskId)
 					print(key)
 				# Verify fingerprints otherwise
 				else:
 					if verifyFingerprint(key, account["identifier"]):
-						if not args.silent:
-							print("PGP key for \"%s\":" % bskId)
+						printInfo("PGP key for \"%s\":" % bskId)
 						print(key)
-					elif not args.silent:
-						print("Couldn't verify fingerprint against key")
+					else:
+						printInfo("Couldn't verify fingerprint against key")
 
-	if not keyFound and not args.silent:
-		print("No PGP keys found for \"%s\"" % bskId)
+	if not keyFound:
+		printInfo("No PGP keys found for \"%s\"" % bskId)
 
 
 # Tries to detect a valid fingerprint, then strips all whitespaces and transforms to upper case
@@ -87,9 +80,8 @@ def cleanFingerprint(fingerprint):
 	else:
 		cleanedFingerprint = None
 
-	if args.debug:
-		print("Given fingerprint:  %s" % fingerprint)
-		print("Cleaned finerprint: %s" % cleanedFingerprint)
+	printDebug("Given fingerprint:  %s" % fingerprint)
+	printDebug("Cleaned finerprint: %s" % cleanedFingerprint)
 
 	return cleanedFingerprint
 
@@ -105,29 +97,35 @@ def verifyFingerprint(keyData, expectedFingerprint):
 		fingerprint = key.fingerprint
 		fingerprint = fingerprint.replace(" ", "")
 	except Exception as e:
-		if not args.silent:
-			print("Error while importing key, probably wasn't able to find a valid key")
-		if args.debug:
-			print("Exception: %s" % e)
+		printInfo("Error while importing key, probably wasn't able to find a valid key")
+		printDebug("Exception: %s" % e)
 		return False
 
 	# Check whether the given fingerprint is long enough to be secure
 	# (unless the check is disabled)
 	if not args.disableLengthCheck and len(expectedFingerprint) < FINGERPRINT_MIN_LEN:
-		if not args.silent:
-			print("Fingerprint \"%s\" is too short to be secure, fingerprint needs more than %s characters" % (expectedFingerprint, FINGERPRINT_MIN_LEN))
+		printInfo("Fingerprint \"%s\" is too short to be secure, fingerprint needs more than %s characters" % (expectedFingerprint, FINGERPRINT_MIN_LEN))
 		return False
 
-	if args.debug:
-		print("Comparing given fingerprint \"%s\" with calculated fingerprint \"%s\"" % (expectedFingerprint, fingerprint))
+	printDebug("Comparing given fingerprint \"%s\" with calculated fingerprint \"%s\"" % (expectedFingerprint, fingerprint))
 
 	# Return whether both (the given and calculated) fingerprints match
 	return fingerprint.endswith(expectedFingerprint)
 
 
+# Prints only when it should (non-silent mode)
+def printInfo(msg):
+	if not args.silent:
+		print(msg)
 
-if args.debug:
-	print("Names to lookup: %s" % args.ids)
+# Prints only when in debug mode
+def printDebug(msg):
+	if args.debug:
+		print(msg)
+
+
+
+printDebug("Names to lookup: %s" % args.ids)
 
 # Cycle through all given IDs and process their respective profile information
 for bskId in args.ids:
